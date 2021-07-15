43ab2956a459fbde9dfd056049f43bd5ede61bcc
shivam472 <shivamraj472@gmail.com>
2021-07-15 22:23:08 +0530

    working with multiple slices

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <>
+      <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      <Counter />
+    </>
   );
 }
 
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,38 @@
-import classes from './Auth.module.css';
+import classes from "./Auth.module.css";
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store";
 
 const Auth = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    const inputIsValid =
+      enteredEmail.trim() !== "" && enteredPassword.trim() !== "";
+    if (inputIsValid) {
+      dispatch(authActions.login());
+      emailInputRef.current.value = "";
+      passwordInputRef.current.value = "";
+    }
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" ref={emailInputRef} />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" ref={passwordInputRef} />
           </div>
           <button>Login</button>
         </form>
